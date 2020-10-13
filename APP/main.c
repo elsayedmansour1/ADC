c5a8391b5520b9c8cea4252b58958b10e8448469
elsayedmansour1 <70556762+elsayedmansour1@users.noreply.github.com>
2020-10-13 19:56:16 +0200

    Add files via upload

---
+++ APP/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 13, 2020
+ *      Author: elsay
+ */
+#include "../LIB/BIT_MATH.h"
+#include "../LIB/STD_TYPES.h"
+#include "../MCAL/DIO/DIO_INT.h"
+
+int main(void)
+{
+	
+	return 0;
+}
